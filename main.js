7b2e6e929cc862754b7f08c35554ac639f4af39a
freetimezp <kraftukrnet@ukr.net>
2022-10-20 21:44:41 +0300

    04. js-portfolio-06-photo. Header section. Animate text as circle.

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 });
+
+//contact btn set text as circle 
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach((textButton) => {
+  let text = textButton.querySelector('p');
+  text.innerHTML = text.innerHTML.split('').map((character, index) => `
+    <span style="transform: rotate(${index * 12}deg)">${character}</span>
+  `).join('');
+});
+
