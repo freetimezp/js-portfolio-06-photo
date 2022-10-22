b0912ba8ac97d2d01be4ea76947edd5ce06a5652
freetimezp <kraftukrnet@ukr.net>
2022-10-22 18:22:28 +0300

    09. js-portfolio-06-photo. Responsive view. Final fix.

--- main.js
+++ main.js
@@ -52,3 +52,29 @@ var swiperWorksCard = new Swiper(".mySwiper-works-2", {
   loop: true,
   grabCursor: true,
 });
+
+//show/ hide navbar
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+const openNav = () => {
+  nav.style.display = 'flex';
+  openNavBtn.style.display = 'none';
+  closeNavBtn.style.display = 'inline-block';
+}
+
+const closeNav = () => {
+  nav.style.display = 'none';
+  openNavBtn.style.display = 'inline-block';
+  closeNavBtn.style.display = 'none';
+}
+
+openNavBtn.addEventListener('click', openNav);
+closeNavBtn.addEventListener('click', closeNav);
+
+if(document.body.clientWidth < 1024) {
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+  })
+}
